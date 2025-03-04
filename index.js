38a0b68c6ea7fdd617b4aec7ec3dffa0c6505080
Prathap-Kabali <prathap.agnostics@gmail.com>
2025-03-04 16:20:32 +0530

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello everyone>>>>, Node.js app is running!");
+    res.send("Hello world>>>>, Node.js app is running!");
 });
 
 app.listen(PORT, () => {
